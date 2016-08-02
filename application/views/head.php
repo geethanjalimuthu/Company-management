d9d2b1237db3d0ca4dc5f331a2ae3afebda8d9db
elavarasan <elamaniam@example.com>
2016-08-02 18:33:08 +0530

    changed the file name head.php

---
+++ application/views/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Tomrain</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="public/css/datepicker.css">
+		<link rel="stylesheet" type="text/css" href="public/css/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script src="public/js/datepicker.js"></script>
+		<script src="public/js/script.js"></script>
+		<script>
+			$(document).ready(function(){
+				$('submit').validate();
+			});
+		</script>
+	</head>
+	<body>
